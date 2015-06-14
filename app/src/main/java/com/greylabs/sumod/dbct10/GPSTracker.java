a99a2468da0fb0e5491b04b0c6c2764c12cab4f4
Sumod Kulkarni <sumodkulkarni@gmail.com>
2015-06-14 18:50:21 +0530

    Location services added. Now the app can extract location data using google location services and in the 'Add' activity it automatically sets the latitude and longitude enabling the user to only enter the category. It also gives a notification to the user if location services are not enabled and provides with a shortcut to directly access the Location settings from within the app.

--- app/src/main/java/com/greylabs/sumod/dbct10/GPSTracker.java
+++ app/src/main/java/com/greylabs/sumod/dbct10/GPSTracker.java
@@ -1,18 +1,5 @@
 package com.greylabs.sumod.dbct10;
 
-import android.app.AlertDialog;
-import android.app.Service;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Bundle;
-import android.os.IBinder;
-import android.provider.Settings;
-import android.util.Log;
-
 /**
  * Created by Sumod on 14-Jun-15.
  */
